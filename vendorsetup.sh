26901b7190da393c22fb014109ed5278565ef160
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2024-01-07 20:53:28 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_a13x-user
-add_lunch_combo twrp_a13x-userdebug
-add_lunch_combo twrp_a13x-eng
+add_lunch_combo omni_a13x-user
+add_lunch_combo omni_a13x-userdebug
+add_lunch_combo omni_a13x-eng
